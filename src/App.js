40a52654182f1e0dffdc78bb9471611a48274fe1
unknown <zakrisson97@gmail.com>
2024-04-04 21:49:03 +0200

    Add React-router functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
+import {Route, Routes} from "react-router-dom"
+import Calendar from "./pages/Calendar"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Welcome to the Homepage!</p>
+      <Routes>
+        <Route path="/calendar" element={<Calendar />} />
+
+      </Routes>
     </div>
   );
 }
