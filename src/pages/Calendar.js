cb4531fc337910ddf3f115ffbb4a9abec23c69e8
unknown <zakrisson97@gmail.com>
2024-04-05 23:57:34 +0200

    Fix comments and fix styling on smaller screen sizes

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -14,7 +14,7 @@ function Calendar() {
   const [showModal, setShowModal] = useState(false)
 
   const fetchData = async () => {
-    {/* If the data is already fetched it will not fetch it again */}
+    // If the data is already fetched it will not fetch it again 
     if (!fetched) {
       setLoading(true);
       try {
@@ -23,7 +23,7 @@ function Calendar() {
         );
         // console.log(response.data);
         setLocations(response.data);
-        {/* If the response is not empty it will set the selected location to Berlin - This was to populate the Calendar Immediately */}
+        // If the response is not empty it will set the selected location to Berlin - This was to populate the Calendar Immediately
         const berlinLocation = response.data.find(location => location.name === 'Berlin')
         if (berlinLocation) {
           setSelectedLocation(berlinLocation.id)
@@ -47,24 +47,25 @@ function Calendar() {
 
   return (
     <>
-    {/*
-     Showing the specific modal for the customer when you press View Booking button. */}
+    
+     {/* Showing the specific modal for the customer when you press View Booking button. */}
       {showModal && <ShowCustomerModal 
       showModal = {showModal}
       handleShowModal = {handleShowModal}
-      customer={selectedCustomer}
       selectedLocation={selectedLocation}
+      customer={selectedCustomer}
       locations={locations}
       />}
-       { /* Dropdown menu for specifying which of the cities you want to see the bookings for. */}
+        {/* Dropdown menu for specifying which of the cities you want to see the bookings for. */}
       <DropDownStation
         loading={loading}
         locations={locations}
         fetchData={fetchData}
         onLocationChange={handleLocationChange}
+        selectedLocation={selectedLocation}
       />
-      <div id="grid-container border-test" className=" grid  lg:grid-cols-7 gap-3">
-        { /* Mapping over the specific days in the calendar */}
+      <div id="grid-container border-test" className=" grid grid-cols-2  xl:grid-cols-7 md:grid-cols-4 gap-1">
+        {/* Mapping over the specific days in the calendar */}
         {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map((day, i) => (
   <div id="border-test" key={i}>
     <div>{day}</div>
@@ -73,19 +74,23 @@ function Calendar() {
         .filter(location => selectedLocation !== null && location.id === selectedLocation)
         .flatMap(location => location.bookings)
         .filter(booking => {
-          {/* Checks if the booking is for the specific day */}
+          // Checks if the booking is for the specific day
           const bookingDate = new Date(booking.startDate);
-          {/* Creates index for the booking date */}
+          // Creates index for the booking date from the day number
           const dayIndex = bookingDate.getDay();
-          {/* Adjuts the day index to start from monday instead of Sunday */}
+          // Adjuts the day index to start from monday instead of Sunday
           const adjustedDayIndex = dayIndex === 0 ? 6 : dayIndex - 1;
           return adjustedDayIndex === i;
         });
-      {/* Maps over the bookings for the specific day and if it's empty it returns No bookings found. */}
+      // Maps over the bookings for the specific day and if it's empty it returns No bookings found.
       return bookingsForDay.length > 0 ? (
         bookingsForDay.map((booking, j) => (
-          <ul id="border-test" className="m-3 p-2" key={j}>
-            <li className="font-sans">{booking.customerName}<br /> {booking.startDate} - {booking.endDate} <br /> </li>
+          <ul id="border-test" className="m-1 p-5" key={j}>
+            <li className="font-sane">ID: {booking.id}</li>
+            <li className="font-sans">{booking.customerName} </li>
+            <li className="font-sans">{booking.startDate} </li>
+            <li className="font-sans">{booking.endDate} </li>
+            {/* Opens specific modal with information of the customer */}
             <Button variant="contained" onClick={() => handleShowModal(booking)}>View Booking</Button>
           </ul>
         ))
@@ -97,6 +102,7 @@ function Calendar() {
 ))}
       </div>
       <div className="flex m-2 gap-2 justify-center">
+      {/* Buttons to navigate between weeks */}
       <Button variant="contained">Prev Week</Button>
       <Button variant="contained">Next Week</Button>
       </div>
