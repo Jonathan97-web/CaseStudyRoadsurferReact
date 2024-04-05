3a6aa7622b45f47bee4a76df5da84b5a191e7369
unknown <zakrisson97@gmail.com>
2024-04-06 00:48:07 +0200

    Add comments

--- src/components/DropDownStation.js
+++ src/components/DropDownStation.js
@@ -1,25 +1,28 @@
 import React, { useEffect } from "react";
 import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
+
+// Dropdown menu for selections of citites for bookings
 const DropDownStation = ({ loading, selectedLocation, locations, fetchData, onLocationChange }) => {
   useEffect(() => {
     fetchData();
   }, [fetchData]);
 
-
-  const handleSelectChange = (event) => {
-    onLocationChange(event.target.value);
+  // Handles the change of the dropdown menu when you select something different
+  const handleSelectChange = (e) => {
+    onLocationChange(e.target.value);
   };
-
-    const location = selectedLocation ? selectedLocation.toString() : '';
+    /* If the selected location is not null it will convert it to a string otherwise it will be an empty string,
+    this is to avoid any errors and value on Select needs to be a string */
+    const selectedLocationValue = selectedLocation ? selectedLocation.toString() : '';
 
 
   return (
     <div>
       <div className="m-2">
         <FormControl fullWidth>
-        <InputLabel>Station</InputLabel>
-        <Select value={location} onChange={handleSelectChange}>
+        <InputLabel></InputLabel>
+        <Select value={selectedLocationValue} onChange={handleSelectChange}>
           {!loading && locations.map((location) => (
             <MenuItem key={location.id} value={location.id}>{location.name}</MenuItem>
           )) } 
