0c66b182c580022f35f6d2d5afc93be1d700e1e8
unknown <zakrisson97@gmail.com>
2024-04-08 18:19:26 +0200

    Add scroll feature to navbar so the border disappears when scrolled

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,6 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import DropDownStation from "./DropDownStation";
 
 export default function NavBar({
@@ -12,29 +10,44 @@ export default function NavBar({
   onLocationChange,
   fetchData,
 }) {
+  const [isScrolled, setIsScrolled] = React.useState(false);
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      const show = window.scrollY > 0;
+      if (isScrolled !== show) {
+        setIsScrolled(show);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isScrolled]);
+
   return (
-    <Box sx={{ flexGrow: 1, padding: 5 }}>
-      <AppBar className="" position="fixed">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: "block", sm: "block" } }}
-          >
-            Booking Calendar
-          </Typography>
-          <DropDownStation
-            sx={{ color: "white" }}
-            locations={locations}
-            fetchData={fetchData}
-            onLocationChange={onLocationChange}
-            selectedLocation={selectedLocation}
-            setSelectedLocation={setSelectedLocation}
-          />
-          <Box sx={{ flexGrow: 1 }} />
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar
+      position="sticky"
+      sx={{
+        background: "transparent",
+        boxShadow: "none",
+        border: isScrolled ? 0 : 2,
+        borderColor: "slategray",
+        borderRadius: 5,
+        width: "80vw",
+        margin: "auto",
+        marginBottom: 2,
+        transition: "border 0.05s ease-in-out",
+      }}
+    >
+      <DropDownStation
+        locations={locations}
+        fetchData={fetchData}
+        onLocationChange={onLocationChange}
+        selectedLocation={selectedLocation}
+        setSelectedLocation={setSelectedLocation}
+      />
+    </AppBar>
   );
 }
