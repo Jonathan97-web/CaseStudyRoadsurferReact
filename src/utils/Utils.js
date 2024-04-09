fb1832846385f080ce04e72bf29189f3a83171af
unknown <zakrisson97@gmail.com>
2024-04-09 23:04:52 +0200

    Create utils file to store functions for calendar

---
+++ src/utils/Utils.js
@@ -0,0 +1,33 @@
+// Utils file for exporting functions to get the week dates and bookings for the week
+
+// Function for getting the week dates
+export const getWeekDates = (weekNumber, startDate) => {
+  const startOfWeek = new Date(startDate);
+  const day = startOfWeek.getDay();
+  const diff = day === 0 ? 6 : day - 1; // Consider Sunday as the end of the week (day 7)
+  startOfWeek.setDate(startOfWeek.getDate() - diff);
+
+  // Add the appropriate number of weeks
+  startOfWeek.setDate(startOfWeek.getDate() + 7 * weekNumber);
+
+  // Calculate the end of the week as 6 days after the start of the week
+  const endOfWeek = new Date(startOfWeek);
+  endOfWeek.setDate(endOfWeek.getDate() + 6);
+
+  return [startOfWeek, endOfWeek];
+};
+
+// Function for getting bookings for the week
+export const getBookingsForWeek = (weekNumber, currentBookings) => {
+  const [startOfWeek, endOfWeek] = getWeekDates(
+    weekNumber,
+    currentBookings[0]?.startDate
+  );
+  return currentBookings.filter((booking) => {
+    const bookingDate = new Date(booking.startDate).getTime();
+    return (
+      bookingDate >= new Date(startOfWeek).getTime() &&
+      bookingDate <= new Date(endOfWeek).getTime()
+    );
+  });
+};
