20a136b4dd4be019e55544a72327cb809893b73a
unknown <zakrisson97@gmail.com>
2024-04-09 23:05:42 +0200

    Change calendar logic to always initalize week with bookings and add alerts for empty weeks

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,104 +1,64 @@
 // Imports of needed modules
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 import "../styles/Calendar.css";
 import { useNavigate } from "react-router-dom";
 import Alert from "@mui/material/Alert";
 import Snackbar from "@mui/material/Snackbar";
+import { getWeekDates, getBookingsForWeek } from "../utils/Utils";
 
 function Calendar({ locations, selectedLocation }) {
   // Navigation hook to navigate to a specific page from React Router
   const navigate = useNavigate();
-  // Start date of the first booking available in API
-  const startDate = new Date("2020-04-02T23:20:49.904Z");
-  // End date in the future (In case new values/bookings are added to the API)
-  const endDate = new Date("2025-12-31T23:20:49.904Z");
   const [showAlert, setShowAlert] = useState(false);
+  const [currentBookings, setCurrentBookings] = useState([]);
+  const [currentWeek, setCurrentWeek] = useState(0);
 
-  // Calculate the first Monday of the year that the startDate falls in
-  const year = startDate.getFullYear();
-  const firstDayOfYear = new Date(year, 0, 1);
-  const firstMondayOfYear = new Date(firstDayOfYear);
-  firstMondayOfYear.setDate(
-    firstDayOfYear.getDate() + ((1 - firstDayOfYear.getDay() + 7) % 7)
-  );
-
-  // Calculate the week number based on the startDate
-  const weekNumber =
-    Math.floor((startDate - firstMondayOfYear) / (7 * 24 * 60 * 60 * 1000)) + 1;
-
-  const [currentWeek, setCurrentWeek] = useState(weekNumber);
-  const totalWeeks = Math.ceil(
-    (endDate - startDate) / (7 * 24 * 60 * 60 * 1000)
-  );
-
-  // getWeekDates function to get the start and end date of the week
-  const getWeekDates = (weekNumber) => {
-    // Calculate the start of the year
-    const year = startDate.getFullYear();
-    const firstDayOfYear = new Date(year, 0, 1);
-
-    // Calculate the first Monday of the year
-    const firstMondayOfYear = new Date(firstDayOfYear);
-    firstMondayOfYear.setDate(
-      firstDayOfYear.getDate() + ((1 - firstDayOfYear.getDay() + 7) % 7)
+  // Get the bookings for the selected location and sort them by date in ascending order
+  useEffect(() => {
+    const currentLocation = locations.find(
+      (location) => location.id === selectedLocation
     );
-
-    // Calculate the start of the week
-    const startOfWeek = new Date(firstMondayOfYear);
-    startOfWeek.setDate(startOfWeek.getDate() + (weekNumber - 1) * 7);
-
-    // Calculate the end of the week
-    const endOfWeek = new Date(startOfWeek);
-    endOfWeek.setDate(endOfWeek.getDate() + 6);
-
-    return [startOfWeek, endOfWeek];
-  };
-  // getBookingsForWeek function to get the bookings for the week
-  const getBookingsForWeek = (weekNumber) => {
-    const [startOfWeek, endOfWeek] = getWeekDates(weekNumber);
-    return locations
-      .filter((location) => location.id === selectedLocation)
-      .flatMap((location) => location.bookings)
-      .filter((booking) => {
-        const bookingDate = new Date(booking.startDate);
-        return bookingDate >= startOfWeek && bookingDate <= endOfWeek;
-      });
-  };
-  // Function to get the next week
-  const nextWeek = () => {
-    let week = currentWeek + 1;
-    while (week < totalWeeks) {
-      if (getBookingsForWeek(week).length > 0) {
-        setCurrentWeek(week);
-        setShowAlert(false);
-        break;
-      }
-      week++;
+    if (currentLocation?.bookings.length > 0) {
+      setCurrentBookings(
+        currentLocation?.bookings.sort(
+          (a, b) =>
+            new Date(a.startDate).getTime() - new Date(b.startDate).getTime()
+        )
+      );
     }
-    if (week >= totalWeeks) {
+  }, [selectedLocation, locations]);
+
+  // Get the bookings for the current week
+  const bookingsForWeek = getBookingsForWeek(currentWeek, currentBookings);
+
+  // Show alert if no bookings are found when pressing next or previous week
+  useEffect(() => {
+    if (bookingsForWeek.length === 0 && currentWeek > 0) {
       setShowAlert(true);
     }
+  }, [currentWeek, bookingsForWeek.length]);
+
+  // Function to get the next week
+  const nextWeek = () => {
+    setCurrentWeek(currentWeek + 1);
   };
   // Function to get the previous week
   const previousWeek = () => {
     let week = currentWeek - 1;
-    while (week >= 0) {
-      if (getBookingsForWeek(week).length > 0) {
-        setCurrentWeek(week);
-        setShowAlert(false);
-        break;
-      }
-      week--;
-    }
-    if (week < 0) {
+    if (currentWeek === 0) {
       setShowAlert(true);
+    } else {
+      setCurrentWeek(week);
+      setShowAlert(false);
     }
   };
 
   // Get the start and end date of the week
-  const [startOfWeek, endOfWeek] = getWeekDates(currentWeek);
-  const bookingsForWeek = getBookingsForWeek(currentWeek);
+  const [startOfWeek, endOfWeek] = getWeekDates(
+    currentWeek,
+    currentBookings[0]?.startDate
+  );
 
   return (
     <>
@@ -110,9 +70,7 @@ function Calendar({ locations, selectedLocation }) {
           autoHideDuration={2000}
           onClose={() => setShowAlert(false)}
         >
-          <Alert severity="info">
-            No more bookings found during these dates.
-          </Alert>
+          <Alert severity="info">No bookings found during these dates.</Alert>
         </Snackbar>
       )}
       <div>
@@ -139,6 +97,7 @@ function Calendar({ locations, selectedLocation }) {
           "Saturday",
           "Sunday",
         ].map((day, i) => {
+          // Filter the bookings for the specific day
           const bookingsForDay = bookingsForWeek.filter((booking) => {
             const bookingDate = new Date(booking.startDate);
             const dayIndex = bookingDate.getDay();
